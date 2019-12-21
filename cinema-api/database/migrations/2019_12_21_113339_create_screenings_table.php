7491c3506965fcf5cc350df784b9fd5689b560da
hadymaher31 <hadymaher31@yahoo.com>
2019-12-21 13:50:56 +0200

    add database tables

---
+++ cinema-api/database/migrations/2019_12_21_113339_create_screenings_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateScreeningsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('screenings', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->dateTime('from');
+            $table->dateTime('to');
+            $table->bigInteger('movie_id')->unsigned();
+            $table->bigInteger('screen_id')->unsigned();
+            $table->foreign('movie_id')->references('id')->on('movies')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('screen_id')->references('id')->on('screens')->onUpdate('cascade')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('screenings');
+    }
+}
