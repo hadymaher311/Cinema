8eaf7b340bd440173216cb5a3f1c184de92c3a9b
hadymaher31 <hadymaher31@yahoo.com>
2019-12-22 13:54:47 +0200

    add navbar dropdown for user name

--- cinema-react/src/components/Layout.jsx
+++ cinema-react/src/components/Layout.jsx
@@ -2,7 +2,7 @@ import React, { Fragment } from "react";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import cookie from "js-cookie";
-import { Navbar, Nav } from "react-bootstrap";
+import { Navbar, Nav, Dropdown } from "react-bootstrap";
 
 function Layout(props) {
   const handleLogout = e => {
@@ -13,44 +13,51 @@ function Layout(props) {
 
   return (
     <div>
-      <Navbar bg="primary" variant="dark">
+      <Navbar collapseOnSelect bg="primary" expand="lg" variant="dark">
         <Navbar.Brand href="/">Cinema</Navbar.Brand>
-        <Nav className="mr-auto">
-          {props.loggedIn && !props.is_admin ? (
-            <React.Fragment>
-              <Nav.Link href="/movies">All Movies</Nav.Link>
-            </React.Fragment>
-          ) : (
-            ""
-          )}
-          {props.loggedIn && props.is_admin ? (
-            <React.Fragment>
-              <Nav.Link href="/admin/users">Users</Nav.Link>
-            </React.Fragment>
-          ) : (
-            ""
-          )}
-        </Nav>
-        <Nav>
-          {!props.loggedIn ? (
-            <Fragment>
-              <Link className="py-1 px-2 text-white" to="/login">
-                Login
-              </Link>
-              <Link className="py-1 px-2 text-white" to="/register">
-                Register
-              </Link>
-            </Fragment>
-          ) : (
-            <Link
-              className="py-1 px-2 text-white"
-              to="/logout"
-              onClick={handleLogout}
-            >
-              Logout
-            </Link>
-          )}
-        </Nav>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto">
+            {props.loggedIn && !props.user.is_admin ? (
+              <React.Fragment>
+                <Nav.Link href="/movies">All Movies</Nav.Link>
+              </React.Fragment>
+            ) : (
+              ""
+            )}
+            {props.loggedIn && props.user.is_admin ? (
+              <React.Fragment>
+                <Nav.Link href="/admin/users">Users</Nav.Link>
+              </React.Fragment>
+            ) : (
+              ""
+            )}
+          </Nav>
+          <Nav>
+            {!props.loggedIn ? (
+              <Fragment>
+                <Link className="py-1 px-2 text-white" to="/login">
+                  Login
+                </Link>
+                <Link className="py-1 px-2 text-white" to="/register">
+                  Register
+                </Link>
+              </Fragment>
+            ) : (
+              <Dropdown>
+                <Dropdown.Toggle id="dropdown-basic">
+                  {props.user.first_name} {props.user.last_name}
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu>
+                  <Dropdown.Item href="/logout" onClick={handleLogout}>
+                    Logout
+                  </Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+            )}
+          </Nav>
+        </Navbar.Collapse>
       </Navbar>
       {props.children}
     </div>
@@ -60,7 +67,7 @@ function Layout(props) {
 const mapStateToProps = state => {
   return {
     loggedIn: state.auth.loggedIn,
-    is_admin: state.auth.user.is_admin
+    user: state.auth.user
   };
 };
 
